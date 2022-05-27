558ce66ae380398d4a5e23297b513cfc5e0633ad
1pete3 <skowronekpeter103@gmail.com>
2022-05-27 16:43:57 -0400

    Scraper works in the forsale section of craigslist now and displays prices

--- main.py
+++ main.py
@@ -10,8 +10,9 @@ def close():
 
 
 def scrape():
+    url = craigslistURL.get()
     # URL of the page being scraped
-    url = "https://hartford.craigslist.org/search/cta"
+
     result = requests.get(url)
     doc = BeautifulSoup(result.text, "html.parser")
 
@@ -19,41 +20,49 @@ def scrape():
     price = doc.findAll("span", {"class": "result-price"})
 
     # Find all occurrences of the h3 tag which gives you the number of listings on a page
-    numOfListings = doc.findAll("h3")
-    print("Number of listings on the page", len(numOfListings))
+    start = doc.find("span",{"class": "rangeFrom"}).text
+    end = doc.find("span",{"class": "rangeTo"}).text
+    totalListings = int(end)-int(start) + 1
+    print("Number of listings on the page", totalListings)
+
+    # Range in the for loop, it shows the amount of times price is listed
+    endOfListing = len(price)
 
     # Removes $ and , from the price
     bad_chars = [',', '$']
 
     # Two lists created, one for storing prices with the $ and , and another without those characters
-    firstPagePrices = []
-    firstPagePricesFiltered = []
+    pagePrices = []
+    pagePricesFiltered = []
     listings = []
 
-    # range is 0 to number of listings but * 2 is necessary since the price appears twice, so it's incremented by 2 to
-    # avoid showing the price twice
-    for x in range(0, len(numOfListings) * 2, 2):
-        firstPagePrices.append(price[x].text)
+    for x in range(0, endOfListing,2):
+        pagePrices.append(price[x].text)
 
-    for y in range(0, len(numOfListings) * 2, 2):
+    for y in range(0, endOfListing,2):
         carprice = price[y].text
         carprice = ''.join(i for i in carprice if not i in bad_chars)
         floatCarPrice = float(carprice)
-        firstPagePricesFiltered.append(floatCarPrice)
+        pagePricesFiltered.append(floatCarPrice)
 
-    print(firstPagePrices)
-    print(firstPagePricesFiltered)
-
-    firstPagePricesFiltered.sort()
-    output = ['{:.2f}'.format(elem) for elem in firstPagePricesFiltered]
+    print(pagePrices)
+    pagePricesFiltered.sort()
+    output = ['{:.2f}'.format(elem) for elem in pagePricesFiltered]
     print(output)
-    print("Cheapest car on the page: $", output[0])
-    print("Most Expensive car on the page: $", output[-1])
+    print("Least Expensive item on the page: $", output[0])
+    print("Most Expensive item on the page: $", output[-1],"\n")
 
 
 win = tkinter.Tk()  # creating the main window and storing the window object in 'win'
-win.geometry('300x100')  # setting the size of the window
+win.geometry('500x200')  # setting the size of the window
 win.title('Craigslist Scraper')
+
+label = Label(win, text='Enter a valid Craigslist URL in the for sale section')
+label.pack(pady=10)
+
+
+craigslistURL = Entry(win,width=50)
+craigslistURL.pack(pady=10)
 
 Button(
     win,
@@ -70,4 +79,6 @@ Button(
     pady=5,
     command=close
 ).pack(pady=5)
+
 win.mainloop()
+
